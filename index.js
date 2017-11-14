55d7b13ef41d58f7168d1cc8b1e8a1336c19c718
Niek Kruse <niekkruse70@gmail.com>
2017-11-14 20:49:52 +0100

    Proxy /messagebird requests with messagebird API

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
+const proxy = require('express-http-proxy');
+
+const MESSAGEBIRD_API_KEY = process.env.MESSAGEBIRD_API_KEY;
+
+const app = express();
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Proxy requests to /messagebird/{resource} 1<>1 with the Messagebird REST API
+// API docs: https://developers.messagebird.com/docs/messaging
+app.use(
+  '/messagebird',
+  proxy('https://rest.messagebird.com/', {
+    proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+      // you can update headers
+      proxyReqOpts.headers['Accept'] = 'application/json';
+      proxyReqOpts.headers[
+        'Authorization'
+      ] = `AccessKey ${MESSAGEBIRD_API_KEY}`;
+      return proxyReqOpts;
+    }
+  })
+);
+
+server.listen(process.env.PORT || 8080, () => {
+  console.log('Listening on %d', server.address().port);
+});
