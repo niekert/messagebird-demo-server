89565aed08d842effb9731f7fb611c280b4cc458
Niek Kruse <niekkruse70@gmail.com>
2017-11-16 21:22:59 +0100

    Add websocket integration for webhook

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const http = require('http');
 const url = require('url');
 const WebSocket = require('ws');
@@ -10,10 +11,33 @@ const MESSAGEBIRD_API_KEY = process.env.MESSAGEBIRD_API_KEY;
 const ALLOWED_ACCESS_KEY = process.env.ALLOWED_ACCESS_KEY;
 
 const app = express();
+
+// TODO: We should probably not whitelist every domain for CORS, but for now it's OK.
 app.use(cors());
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+// Super basic way to ensure request sent without the accesKey in the
+// authorization header is denied
+app.use('*', (req, res, next) => {
+  // TODO: Hacky way to not block requests where we receive the message callback from the
+  // Messagebird API. Instead of doing this we should probably check the host domain from which
+  // The request was received
+  if (req.originalUrl === '/message') {
+    next();
+    return;
+  }
+
+  const accessKey = req.headers.authorization;
+  if (accessKey !== `AccessKey ${ALLOWED_ACCESS_KEY}`) {
+    res.sendStatus(401);
+    return;
+  }
+
+  next();
+});
 
 // Proxy requests to /messagebird/{resource} 1<>1 with the Messagebird REST API
 // API docs: https://developers.messagebird.com/docs/messaging
@@ -31,16 +55,29 @@ app.use(
   })
 );
 
-// Super basic way to ensure request sent without the accesKey in the
-// authorization header is denied
-app.use('/', (req, res, next) => {
-  const accessKey = req.headers.authorization;
-  console.log('access', accessKey, ALLOWED_ACCESS_KEY);
-  if (accessKey !== `AccessKey ${ALLOWED_ACCESS_KEY}`) {
-    res.send(401);
+// Simple endpoint to verify if the given API token is valid.
+app.get('/authenticate', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post('/message', (req, res) => {
+  const body = req.body;
+
+  // TODO: We should probably do a more thorough check here before sending it
+  if (body.type === 'sms') {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: 'message', data: body }));
+      }
+    });
   }
 
-  next();
+  res.sendStatus(200);
+});
+
+// Send a simple OK message when the websocket connects
+wss.on('connection', ws => {
+  ws.send(JSON.stringify({ type: 'connect' }));
 });
 
 server.listen(process.env.PORT || 8080, () => {
